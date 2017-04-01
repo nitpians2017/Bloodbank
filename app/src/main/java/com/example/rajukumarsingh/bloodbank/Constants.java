982de173cd8fa0172bd52417585099a16d7959d6
Sagar <sagar@bizonesoft.com>
2017-04-01 12:34:16 +0530

    URL changed

--- app/src/main/java/com/example/rajukumarsingh/bloodbank/Constants.java
+++ app/src/main/java/com/example/rajukumarsingh/bloodbank/Constants.java
@@ -6,12 +6,13 @@ package com.example.rajukumarsingh.bloodbank;
 
 public class Constants {
 
-    public static final String ACCEPTOR_LOGIN_URL = "http://rahulraj47.coolpage.biz/Acceptor_login.php";
-    public static final String DONOR_CITY_SEARCH = "http://rahulraj47.coolpage.biz/donor2.php";
-    public static final String ACCEPTOR_SIGNUP = "http://rahulraj47.coolpage.biz/ACCEPTOR.php";
-    public static final String GPS_FETCH = "http://rahulraj47.coolpage.biz/gps_fetch.php";
-    public static final String BLOOD_BANK_LOGIN = "http://rahulraj47.coolpage.biz/blood_bank_login.php";
-    public static final String BLOOD_BANK_SIGNUP = "http://rahulraj47.coolpage.biz/blood_bank.php";
-    public static final String DONOR_SIGNUP = "http://rahulraj47.coolpage.biz/donor1.php";
-    public static final String DONOR_LOGIN = "http://rahulraj47.coolpage.biz/donor_login.php";
+    public static final String URL = "http://hackathon.bizonesoft.com/";
+    public static final String ACCEPTOR_LOGIN_URL = URL + "Acceptor_login.php";
+    public static final String DONOR_CITY_SEARCH = URL + "donor2.php";
+    public static final String ACCEPTOR_SIGNUP = URL + "ACCEPTOR.php";
+    public static final String GPS_FETCH = URL + "gps_fetch.php";
+    public static final String BLOOD_BANK_LOGIN = URL + "blood_bank_login.php";
+    public static final String BLOOD_BANK_SIGNUP = URL + "blood_bank.php";
+    public static final String DONOR_SIGNUP = URL + "donor1.php";
+    public static final String DONOR_LOGIN = URL + "donor_login.php";
 }
